cbfa4940682c053b02106f97f72cbc9e86bc0477
Eben Gunadi <egunadi@usc.edu>
2023-09-30 21:10:17 -0700

    Update shellSettings/zshrc.sh

--- shellSettings/zshrc.sh
+++ shellSettings/zshrc.sh
@@ -2,19 +2,19 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/Users/egunadi/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-# ZSH_THEME="robbyrussell"
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="af-magic"
 # ZSH_THEME="avit"
+# ZSH_THEME="robbyrussell"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in ~/.oh-my-zsh/themes/
+# a theme from this variable instead of looking in $ZSH/themes/
 # If set to an empty array, this variable will have no effect.
 # ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
 
@@ -25,17 +25,16 @@ ZSH_THEME="af-magic"
 # Case-sensitive completion must be off. _ and - will be interchangeable.
 # HYPHEN_INSENSITIVE="true"
 
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment the following line to automatically update without prompting.
-# DISABLE_UPDATE_PROMPT="true"
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
 
 # Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
+# zstyle ':omz:update' frequency 13
 
 # Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS=true
+# DISABLE_MAGIC_FUNCTIONS="true"
 
 # Uncomment the following line to disable colors in ls.
 # DISABLE_LS_COLORS="true"
@@ -47,6 +46,9 @@ ZSH_THEME="af-magic"
 # ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
 # COMPLETION_WAITING_DOTS="true"
 
 # Uncomment the following line if you want to disable marking untracked files
@@ -66,8 +68,8 @@ ZSH_THEME="af-magic"
 # ZSH_CUSTOM=/path/to/new-custom-folder
 
 # Which plugins would you like to load?
-# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(git)
@@ -99,49 +101,7 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias d='cd ~/Documents/'       # Navigate to Documents dir
-alias r='cd ~/Documents/References/'      # Navigate to References dir
-alias t='cd ~/Desktop/'       # Navigate to Desktop dir
-alias b='cd ~/Documents/Resources/Books/'       # Navigate to Books dir
-alias g='cd ~/Git/'         # Navigate to Git dir
-alias gd='cd ~/Google\ Drive/'         # Navigate to Google Drive dir
-alias c='clear'                             # c:            Clear terminal display
-alias rpt='cd ~/Git/mi-reporting-mips/'  # Navitage to mi-reporting-mips repo
-alias 77='cd ~/Git/mi-core-sql-v77/' # Navitage to mi-core-sql-v77 repo
-alias ps='cd ~/Git/pstoolbox/mi-core/' # Navitage to PS scripts folder
-pg() {
-  open -a postgres
-  open -a "pgAdmin 4"
-}
-pgx() {
-  osascript -e 'quit app "postgres"'
-  osascript -e 'quit app "pgAdmin 4"'
-  osascript -e 'quit app "PostgresMenuHelper"'
-}
-sql() {
-  open -a Docker
-  open -a "Azure Data Studio"
-}
-sqlx() {
-  osascript -e 'quit app "Azure Data Studio"'
-  osascript -e 'quit app "Docker"'
-}
-ch() {
-  open -a "Google Chrome" $1
-}
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/opt/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
+alias d='cd ~/Documents/' 
+alias t='cd ~/Desktop/'
+alias g='cd ~/Git/'
+alias c='clear'
