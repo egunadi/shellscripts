9cd49aef522480227224e7c77ceda52c5ba864a1
Eben Gunadi <egunadi@usc.edu>
2025-04-21 19:27:56 -0700

    nvim

--- neovim/config/keymaps.lua
+++ neovim/config/keymaps.lua
@@ -7,7 +7,26 @@ vim.keymap.set("i", "JK", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "KJ", "<Esc>", { desc = "Exit insert mode" })
 
 -- Remove any previous mapping
-vim.api.nvim_del_keymap("i", "<CR>")
+pcall(vim.api.nvim_del_keymap, "i", "<CR>")
 
 -- Force Enter to insert a newline only
 vim.keymap.set("i", "<CR>", "<CR>", { noremap = true, silent = true, desc = "Insert newline" })
+
+if vim.g.neovide then
+  -- Set the initial scale factor
+  vim.g.neovide_scale_factor = 1.0
+
+  local function change_scale_factor(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+
+  -- Zoom in with Ctrl + =
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end, { desc = "Zoom In (Neovide)" })
+
+  -- Zoom out with Ctrl + -
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end, { desc = "Zoom Out (Neovide)" })
+end
